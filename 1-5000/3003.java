ee7c5f8b4fc0c53f5dd9c5f743a0563f30457ef9
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-01-04 23:20:02 +0900

    Update 3003.java

--- 1-5000/3003.java
+++ 1-5000/3003.java
@@ -3,26 +3,11 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 			Scanner scan = new Scanner(System.in);
+			int[] arr = {1,1,2,2,2,8};
 			
-			int a = scan.nextInt();
-			int b = scan.nextInt();
-			int c = scan.nextInt();
-			int d = scan.nextInt();
-			int e = scan.nextInt();
-			int f = scan.nextInt();
-			
-			int a1 = 1 - a;
-			int b1 = 1 - b;
-			int c1 = 2 - c;
-			int d1 = 2 - d;
-			int e1 = 2 - e;
-			int f1 = 8 - f;
-			
-			System.out.print(a1 + " ");
-			System.out.print(b1 + " ");
-			System.out.print(c1 + " ");
-			System.out.print(d1 + " ");
-			System.out.print(e1 + " ");
-			System.out.print(f1 + " ");
+			for(int i=0; i<arr.length; i++) {
+				arr[i] -= scan.nextInt();
+				System.out.print(arr[i] + " ");
+			}
 	}
 }
