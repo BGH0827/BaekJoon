e78c5463a32780e5fcf3ed28b7017a3a54f0308c
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-01-18 18:21:09 +0900

    Create 10950.java

---
+++ 10001-15000/10950.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<a; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			sb.append(b+c);
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
