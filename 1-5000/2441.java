4761026fddc37ccf4d86aec2fd376c6f8c00702f
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-01-18 20:02:10 +0900

    Create 2441.java

---
+++ 1-5000/2441.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<a; i++) {
+			for(int j=0; j<i; j++) {
+				sb.append(" ");
+			}
+			for(int k=a ; k>i; k--) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
