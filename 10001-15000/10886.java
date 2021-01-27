a6325c3164772cce80165e8433d801c5ad5895dc
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-01-28 00:04:22 +0900

    Update 10886.java

--- 10001-15000/10886.java
+++ 10001-15000/10886.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		int a = Integer.parseInt(br.readLine());
 		int count = 0;
@@ -20,11 +17,11 @@ public class Main {
 			else
 				count1++;
 		}
-		if(count > count1) {
-			sb.append("Junhee is not cute!");
-		}
-		else
+		if(count > count1)
 			sb.append("Junhee is cute!");
-
+		else
+			sb.append("Junhee is not cute!");
+		
+		System.out.print(sb);
 	}
 }
