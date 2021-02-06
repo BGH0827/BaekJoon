d9fc85d600e599fa25168fa07c69ee1aad50cbe2
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-02-06 21:12:32 +0900

    Update 1000.java

--- 1-5000/1000.java
+++ 1-5000/1000.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) extends NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStream
-		int A = scan.nextInt();
-		int B = scan.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = br.readLine();
+		int B = br.readLine();
 		
 		int sum = A + B;
 		System.out.println(sum);
