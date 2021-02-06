d6eca53592f97e60164a87cb57c6b97449171243
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-02-06 21:11:36 +0900

    Update 1000.java

--- 1-5000/1000.java
+++ 1-5000/1000.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) extends {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) extends NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStream
 		int A = scan.nextInt();
 		int B = scan.nextInt();
 		
