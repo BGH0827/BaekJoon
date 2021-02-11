b77c209c2e5b604f573d0696b5a12dd17067ee7c
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-02-11 22:38:38 +0900

    Update 15962.java

--- 15001-20000/15962.java
+++ 15001-20000/15962.java
@@ -1,5 +1,5 @@
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) extends NumberFormatException, IOException {
 		StringBuilder sb = new StringBuilder();
 		sb.append("파이팅!!");
 		System.out.print(sb);
