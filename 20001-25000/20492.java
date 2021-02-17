cab7707e0394867057c0d45346297e69f84b4ecc
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-02-17 19:50:50 +0900

    Update 20492.java

--- 20001-25000/20492.java
+++ 20001-25000/20492.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int a = Integer.parseInt(br.readLine());
 
 		System.out.println(78*a/100 + " " + ((80*a/100)+(78*(20*a/100)/100)));
