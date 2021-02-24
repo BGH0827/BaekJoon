b3882b722bbc6e98d3dff178e48d0c0a90d29476
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-02-24 23:37:45 +0900

    Update 10430.java

--- 10001-15000/10430.java
+++ 10001-15000/10430.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a[] = new int[5];
-
-		Scanner scan = new Scanner(System.in);
 		
-		int A = scan.nextInt();
-		int B = scan.nextInt();
-		int C = scan.nextInt();
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
 		
 		a[0] = (A+B)%C;
 		a[1] = ((A%C)+(B%C))%C;
