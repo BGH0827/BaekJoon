eb88080abc4c7eccb5b11e425372f696c1ff1296
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-02-25 23:58:49 +0900

    Update 10170.java

--- 10001-15000/10170.java
+++ 10001-15000/10170.java
@@ -1,5 +1,5 @@
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IoException {
 
 		System.out.println("NFC West       W   L  T");
 		System.out.println("-----------------------");
