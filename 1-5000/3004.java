58e569cefe0fd477027f0fcee38e601848a85a99
BGH0827 <74364833+BGH0827@users.noreply.github.com>
2021-03-05 19:55:56 +0900

    Create 3004.java

---
+++ 1-5000/3004.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int x = a / 2;
+		int y = a - x;
+		System.out.print((x+1)*(y+1));
+	}
+}
